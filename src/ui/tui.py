6cde667461b80ac6b3be25e377383830ef6b4c33
davinjason09 <davinjasonevanraharjo@mail.ugm.ac.id>
2026-01-13 13:44:36 +0700

    feat(ui/tui): TUI utilities (console, logger, print title, print options)

---
+++ src/ui/tui.py
@@ -0,0 +1,70 @@
+import logging
+import random
+from logging import Logger
+
+from rich.console import Console
+from rich.logging import RichHandler
+
+console = Console()
+
+
+def setup_logger(level="INFO") -> Logger:
+    logging.basicConfig(
+        level=level,
+        format="%(message)s",
+        datefmt="[%X]",
+        handlers=[RichHandler(console=console, rich_tracebacks=True, markup=True)],
+    )
+
+    return logging.getLogger("kkn_app")
+
+
+log = setup_logger("WARN")
+
+
+def print_title():
+    title = [
+        "[#99FF99]▄▄▄    ▄▄▄          ████                         ▄▄▄  ▄▄▄ ▄▄▄  ▄▄▄ ▄▄▄  ▄▄▄[/]",
+        "[#99FFB2]███▄  ▄███  ██████▄  ███   ██████▄ ▄███████      ███  ███ ███  ███ ███▄ ███[/]",
+        "[#99FFCC]██████████ ▄▄▄▄▄███  ███  ▄▄▄▄▄███ ▀██▄▄▀▀▀ ▄▄▄▄ ████▄██▀ ████▄██▀ ████▄███[/]",
+        "[#99FFE5]███ ▀▀ ███ ███▀▀███  ███  ███▀▀███   ▀▀███▄ ▀▀▀▀ ███▀███▄ ███▀███▄ ███▀████[/]",
+        "[#99FFFF]███    ███ ▀███████ █████ ▀███████ ███████▀      ███  ███ ███  ███ ███ ▀███[/]",
+    ]
+
+    splash_text = [
+        "Because life is too short for manual logbook",
+        "I don't have enough time to deal with this sh*t",
+        "Imagine doing this manually through the web, lmao",
+        "Speedrunning KKN Administrative Tasks (Any%)",
+        "Constructing payload... Target locked... Attendance posted",
+        "Powered by caffeine and hatred for legacy code",
+        "Who's in the right mind sending back a 100kb HTML file??",
+        "Does anyone actually read these logbooks? asking for a script.",
+        "Generating 'productive' activity descriptions...",
+        "Fake it 'til you automate it.",
+    ]
+
+    random_quotes = f"\n{random.choice(splash_text):^75}\n"
+    console.print(("\n".join(title)))
+    console.print(random_quotes)
+
+
+def print_choice():
+    options = [
+        "Post Daily Attendance",  # done
+        "Show Programs",  # done
+        "Add New Logbook Entry (My Program)",  # hook this
+        "Add New Sub-Entry (My Program)",  # test this
+        "Post Attendance for Unattended Entries",
+        "Generate Activity Timeline",
+        "Change Account",
+        "Refresh",
+        "Exit",
+    ]
+
+    opt_len = len(str(len(options)))
+    for i, opt in enumerate(options, 1):
+        fmt_opt = f"[cyan][[#fab387]{i:0{opt_len}}[/]][/] {opt}"
+        console.print(fmt_opt)
+
+    print()
