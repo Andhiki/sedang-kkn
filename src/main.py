3e7f1f8fc6f7b0e53fc48dadbf6c5620a31a252e
davinjason09 <davinjasonevanraharjo@mail.ugm.ac.id>
2026-01-21 16:22:05 +0700

    feat(main): update attendance logic, better UX when handling exceptions

--- src/main.py
+++ src/main.py
@@ -6,8 +6,8 @@ from prompt_toolkit import HTML
 from tap import Tap
 
 import actions
-from utils.attendance import handle_check_status, handle_checkin
 from ui.tui import console, print_choice, print_log, print_title, prompt_session
+from utils.attendance import handle_attendance, handle_check_status
 from utils.common import async_input
 from utils.kkn import KKN
 from utils.simaster import Simaster
@@ -45,43 +45,54 @@ async def main_async(username: str, password: str):
     )
     print()
 
-    if choice == "1":
-      handle_checkin(username, password)
-    elif choice == "2":
-      await actions.show_all_program(kkn_manager)
-    elif choice == "3":
-      await actions.add_new_entry(kkn_manager)
-    elif choice == "4":
-      await actions.add_new_sub_entry(kkn_manager)
-    elif choice == "5":
-      await actions.handle_unattended_entries(kkn_manager)
-      pass
-    elif choice == "6":
-      # await load_background("[blue]Background fetch in progress...[/]", kkn_manager.loader)
-      # TODO: handle report generation
-      console.print("[#181825 on cyan] TODO [/] Report generation")
-      pass
-    elif choice == "7":
-      if result := await actions.change_account():
-        simaster_acc, session, kkn_manager = result
-        username = simaster_acc.username
-        password = simaster_acc.password
-    elif choice == "8":
-      kkn_manager.loader = asyncio.create_task(kkn_manager._load_all(kkn_manager.simaster_account))
-      console.print("[blue]Data refresh started in background...")
-    elif choice == "9":
-      console.print("[yellow]Exiting...[/]")
-      if not kkn_manager.loader.done():
-        kkn_manager.loader.cancel()
-      break
-    else:
-      print_log("Invalid Choice. Please try again")
+    try:
+      if choice == "1":
+        handle_attendance(username, password, "check_in")
+      elif choice == "2":
+        await actions.show_all_program(kkn_manager)
+      elif choice == "3":
+        await actions.add_new_entry(kkn_manager)
+      elif choice == "4":
+        await actions.add_new_sub_entry(kkn_manager)
+      elif choice == "5":
+        await actions.handle_unattended_entries(kkn_manager)
+        pass
+      elif choice == "6":
+        # await load_background("[blue]Background fetch in progress...[/]", kkn_manager.loader)
+        # TODO: handle report generation
+        console.print("[#181825 on #89dceb] TODO [/] Report generation")
+        pass
+      elif choice == "7":
+        if result := await actions.change_account():
+          simaster_acc, session, kkn_manager = result
+          username = simaster_acc.username
+          password = simaster_acc.password
+      elif choice == "8":
+        kkn_manager.loader = asyncio.create_task(kkn_manager._load_all(kkn_manager.simaster_account))
+        console.print("[blue]Data refresh started in background...")
+      elif choice == "9":
+        console.print("[yellow]Exiting...[/]")
+        if not kkn_manager.loader.done():
+          kkn_manager.loader.cancel()
+        break
+      else:
+        print_log(f"Invalid Choice ({choice}). Please try again")
 
-    with console.status("Press Enter to return to the main menu...", spinner="dots"):
-      input()
+      with console.status("Press Enter to return to the main menu...", spinner="dots"):
+        await async_input()
 
-    # HACK: we need to remove the spinner somehow since it doesn't work with input()...
-    print("\033[A\033[K")
+      # HACK: we need to remove the spinner somehow since it doesn't work with input()...
+      print("\033[A\033[K")
+    except (KeyboardInterrupt, asyncio.CancelledError, EOFError):
+      print()
+      print_log("Action interrupted! returning to Main Menu[#89dceb]...")
+      print()
+
+
+def signal_handler(_sig, _frame):
+  print()
+  print_log("Program interrupted by user, exiting...")
+  os._exit(0)
 
 
 def main():
@@ -94,11 +105,15 @@ def main():
   )
 
   if args.submit:
-    handle_checkin(username, password)
+    handle_attendance(username, password, "check_in")
   elif args.check:
     handle_check_status(username, password)
   else:
-    asyncio.run(main_async(username, password))
+    try:
+      asyncio.run(main_async(username, password))
+    except KeyboardInterrupt:
+      print()
+      print_log("Program interrupted! Exiting[#89dceb]...")
 
 
 if __name__ == "__main__":
